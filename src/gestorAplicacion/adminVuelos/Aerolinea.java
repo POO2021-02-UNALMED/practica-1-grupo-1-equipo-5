d57cb1fa7d9d29088ad4819d9e4f3deedf933945
Karenrivgi <kariverag@unal.edu.co>
2021-12-10 22:00:54 -0500

    cambio de paquete Clase Aerolinea y cambios

--- src/gestorAplicacion/adminVuelos/Aerolinea.java
+++ src/gestorAplicacion/adminVuelos/Aerolinea.java
@@ -1,88 +1,97 @@
-package gestorAplicacion;
-
+package gestorAplicacion.adminVuelos;
+import java.io.Serializable;
 import java.util.ArrayList;
+import gestorAplicacion.hangar.*;
 
-public class Aerolinea {
+public class Aerolinea implements Serializable{
 
-	
+	private static final long serialVersionUID = 1L;
 	private String nombre;
 	private ArrayList<Aeronave> aeronaves = new ArrayList<Aeronave>();
 	private ArrayList<Vuelo> vuelos = new ArrayList<Vuelo>();
 	private static ArrayList<Aerolinea> aerolineas = new ArrayList<Aerolinea>();
-	
+
 	public Aerolinea(String nombre) {
 		this.nombre = nombre;
 		aerolineas.add(this);
 	}
-	
-	// REGISTRAR AVION EN AEROLINEA()
-	public void registrarAeronave(Aeronave aeronave) {
-		
-	}
-	
+
 	// AGREGAR AEROLINEA
 	public static void agregarAerolinea(String nombre,ArrayList<Aeronave> aeronaves,ArrayList<Vuelo> vuelos) {
 		Aerolinea nueva_aerolinea = new Aerolinea(nombre);
 		nueva_aerolinea.setAviones(aeronaves);
 		nueva_aerolinea.setVuelos(vuelos);
 	}
-	
-	
+
+
 	//BUSCAR AEROLINEA
 	public static Aerolinea buscarAerolineaPorNombre(String nombre2)
 	{
+		Aerolinea retorno = null;
 		for (int i = 0; i < Aerolinea.getAerolineas().size(); i++)
 		{
-			if( Aerolinea.getAerolineas().get(i).getNombre().equals(nombre2))
+			if( Aerolinea.getAerolineas().get(i).getNombre().equalsIgnoreCase(nombre2))
 			{
-				
-				return  Aerolinea.getAerolineas().get(i);
+
+				retorno=  Aerolinea.getAerolineas().get(i);
 			}
 		}
-		return null;
+		return retorno;
 	}
-	
+
 	//BUSCAR VUELO POR...
-	public Vuelo buscarVueloPorID (ArrayList<Vuelo> vuelos, int ID) 
+	public Vuelo buscarVueloPorID (ArrayList<Vuelo> vuelos, int ID)
 	{
-		for (int i = 0; i < vuelos.size(); i++) 
+		for (int i = 0; i < vuelos.size(); i++)
 		{
 		  if (vuelos.get(i).getID() == ID )
 		  {
 			  return vuelos.get(i);
-		  }  
-		} 
+		  }
+		}
 		return null;
 	}
-		
-	public ArrayList<Vuelo> buscarVueloPorDestino (ArrayList<Vuelo> vuelos, String destino) 
+
+	public Vuelo buscarVueloPorAeronave (ArrayList<Vuelo> vuelos, String nombre_Aeronave)
+	{
+		for (int i = 0; i < vuelos.size(); i++)
+		{
+		  if (vuelos.get(i).getAeronave().getNombre().equals(nombre_Aeronave) )
+		  {
+			  return vuelos.get(i);
+		  }
+		}
+		return null;
+	}
+
+	public ArrayList<Vuelo> buscarVueloPorDestino (ArrayList<Vuelo> vuelos, String destino)
 	{
 		ArrayList<Vuelo> vuelosPorDestino = new ArrayList<Vuelo>();
-		for (int i = 0; i < vuelos.size(); i++) 
+		for (int i = 0; i < vuelos.size(); i++)
 		{
-		  if (vuelos.get(i).getDestino().equals(destino))
+		  if (vuelos.get(i).getDestino().equalsIgnoreCase(destino)) // cambio del equals
 		  {
-			  vuelosPorDestino.add(vuelos.get(i)); 
-		  }  
+			  vuelosPorDestino.add(vuelos.get(i));
+		  }
 		}
-		return vuelosPorDestino; 
+		return vuelosPorDestino;
 	}
-	
-	 
-	
-	public ArrayList<Vuelo> buscarVueloPorFecha (ArrayList<Vuelo> vuelos, String fecha) 
+
+
+
+	public ArrayList<Vuelo> buscarVueloPorFecha (ArrayList<Vuelo> vuelos, String fecha)
 	{
 		ArrayList<Vuelo> vuelosPorFecha = new ArrayList<Vuelo>();
-		for (int i = 0; i < vuelos.size(); i++) 
+		for (int i = 0; i < vuelos.size(); i++)
 		{
 		  if (vuelos.get(i).getFecha_de_salida().equals(fecha))
 		  {
-			  vuelosPorFecha.add(vuelos.get(i)); 
-		  }  
+			  vuelosPorFecha.add(vuelos.get(i));
+		  }
 		}
-		return vuelosPorFecha; 
+		return vuelosPorFecha;
 	}
-	
+
 	// BUSCAR TIQUETE POR ID
 	public static Tiquete BuscarTiquete(int ID)
 	{
@@ -92,23 +101,23 @@ public class Aerolinea {
 			Aerolinea aerolinea = aerolineasDisponibles.get(i);
 			for (int j = 0; j < aerolinea.getVuelos().size(); j++)
 			{
-				
+
 				Vuelo vuelo = aerolinea.getVuelos().get(j);
 				Tiquete tiquete_buscado = vuelo.buscarTiquetePorID(vuelo.getTiquetes(), ID);
 				if (tiquete_buscado != null)
 				{
 					return tiquete_buscado;
-				}	
-			}			
+				}
+			}
 		}
 		return null;
 	}
-	
+
 	//VUELOS DISPONIBLES
 	public ArrayList<Vuelo> vuelosDisponibles(ArrayList<Vuelo> vuelos)
 	{
 		ArrayList<Vuelo> vuelosDisponibles = new ArrayList<Vuelo>();
-		for (int i = 0; i < vuelos.size(); i++) 
+		for (int i = 0; i < vuelos.size(); i++)
 		{
 			if (!vuelos.get(i).isEstaCompleto())
 			{
@@ -117,28 +126,28 @@ public class Aerolinea {
 		}
 		return vuelosDisponibles;
 	}
-	
+
 	// AGREGAR O CANCELAR UN VUELO
-	public void agregarVuelo(Vuelo vuelo) 
+	public void agregarVuelo(Vuelo vuelo)
 	{
 		vuelos.add(vuelo);
 	}
-	
+
 	public Boolean cancelarVuelo(int vuelo_a_eliminar)
 	{
-		for (int i = 0; i < vuelos.size(); i++) 
+		for (int i = 0; i < vuelos.size(); i++)
 		{
 		  if (vuelos.get(i).getID() == vuelo_a_eliminar )
 		  {
 			  vuelos.remove(i);
 			  return true;
-		  }  
+		  }
 		}
 		return false;
 	}
-	
+
 	// SETTERS Y GETTERS
-	
+
 	public String getNombre() {
 		return nombre;
 	}
@@ -165,7 +174,7 @@ public class Aerolinea {
 	public void setAviones(ArrayList<Aeronave> aviones) {
 		this.aeronaves = aviones;
 	}
-	
+
 	public static ArrayList<Aerolinea> getAerolineas() {
 		return aerolineas;
 	}
@@ -173,5 +182,5 @@ public class Aerolinea {
 	public static void setAerolineas(ArrayList<Aerolinea> aerolineas) {
 		Aerolinea.aerolineas = aerolineas;
 	}
-	
+
 }
