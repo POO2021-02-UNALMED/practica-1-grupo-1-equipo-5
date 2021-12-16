442ec99659c2e59680fce40967dcb62fb05d87f0
jero55 <jsalazars@unal.edu.co>
2021-12-15 21:41:38 -0500

    Serializador y casos de prueba

--- src/baseDatos/Serializador.java
+++ src/baseDatos/Serializador.java
@@ -1,5 +1,66 @@
 package baseDatos;
 
-public class Serializador {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 
+import gestorAplicacion.Alojamiento.Alojamiento;
+import gestorAplicacion.adminVuelos.Aerolinea;
+
+public class Serializador {
+	private static File rutaArchivosTemp = new File("src"+File.separator+"basedatos"+File.separator+"temp");
+	
+	// SERIALIZAMOS LAS LISTAS DE VUELO Y AERONAVE QUE ESTAN EN CADA AEROLINEA
+	public static void serializar() {
+		FileOutputStream rutaArchivo; 
+		ObjectOutputStream fichero_objeto; 
+		File[] ficheros = rutaArchivosTemp.listFiles(); 
+		PrintWriter pw;
+		
+
+		// CON PRINTWRITER BORRAMOS EL CONTENIDO PARA EVITAR SOBREESCRITURAS 
+		for (File archivo : ficheros) { 
+			try {
+				//BORRA LO QUE HAY EN EL ARCHIVO QUE LE PASAMOS
+				pw = new PrintWriter(archivo);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		
+
+		
+		
+		for (File archivo1 : ficheros) {
+
+			if (archivo1.getAbsolutePath().contains("Aerolineas")) { 
+				try {
+					rutaArchivo = new FileOutputStream(archivo1); 
+					fichero_objeto = new ObjectOutputStream(rutaArchivo);
+					fichero_objeto.writeObject(Aerolinea.getAerolineas()); 
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace(); 
+				} catch (IOException e) {
+					// TODO Auto-generated catch block e.printStackTrace();
+					e.printStackTrace(); 
+				}
+			}else if(archivo1.getAbsolutePath().contains("Alojamientos")) { 
+				try {
+					rutaArchivo = new FileOutputStream(archivo1); 
+					fichero_objeto = new ObjectOutputStream(rutaArchivo);
+					fichero_objeto.writeObject(Alojamiento.getAlojamientos());
+				}catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace(); 
+				} catch (IOException e) {
+					// TODO Auto-generated catch block e.printStackTrace();
+					e.printStackTrace();
+			}
+		}
+	}  
+}
 }
--- src/uiMain/Admin.java
+++ src/uiMain/Admin.java
@@ -18,6 +18,56 @@ public class Admin {
 	public static void main(String[] args) {
 	//  CASOS DE PRUEBA !!!		
 		 
+		
+		Deserializador.deserializar();
+		
+		int opcion;
+		do {
+				System.out.println("---- SISTEMA DE RESERVAS DE VUELO ---");
+				System.out.println("Que operacion desea realizar?");
+				System.out.println("1. Ver todos los vuelos disponibles por Aerolinea");
+				System.out.println("2. Comprar tiquete para un vuelo por destino y fecha");
+				System.out.println("3. Agregar alojamiento en el destino del vuelo");
+				System.out.println("4. Modificar tiquete comprado");
+				System.out.println("5. Ver opciones de administrador");
+				System.out.println("6. Terminar");
+				System.out.println("Por favor escoja una opcion: ");
+				opcion = sc.nextInt();
+				
+				switch (opcion) {
+					case 1: ;
+						mostrarVuelosPorAerolineas();
+						break;
+					case 2:
+						generarTiquete();
+						break;
+					case 3:
+						agregarAlojamiento();
+						break;
+					case 4:
+						modificarTiquete();
+						break;
+					case 5: 
+						opcionesAdministrador(); 
+						break;
+					case 6:
+						salirDelSistema();
+					
+						break;
+				}		
+		}while (opcion != 6);
+	}
+		
+	// CASE 1 MAIN: VER TODOS LOS VUELOS DISPONIBLES POR AEROLINEAS	
+	
+		// MUESTRA UNA TABLA POR CADA AEROLINEA CON LOS VUELOS QUE SE TIENEN DISPONIBLES, HACIENDO USO DE LOS METODOS PRINTENCABEZADO()
+		// PRINTVUELOS() Y PRINTSEPARADOR()
+		static void mostrarVuelosPorAerolineas()
+		{
+			ArrayList<Aerolinea> aerolineasDisponibles = Aerolinea.getAerolineas();
+			generadorDeTablas.mostrarTablaDeVuelosDisponiblesPorAerolineas(aerolineasDisponibles);
+		}
+		
 
 }
-	}
+
