32858fa822d97ebc7604e41860cc10c50ff451c7
Karenrivgi <kariverag@unal.edu.co>
2021-12-15 21:44:11 -0500

    Create GeneradorDeTablas.java
    Interface implementada

---
+++ src/uiMain/GeneradorDeTablas.java
@@ -0,0 +1,13 @@
+package uiMain;
+import java.util.ArrayList;
+import gestorAplicacion.adminVuelos.*;
+import gestorAplicacion.Alojamiento.*;
+
+public interface GeneradorDeTablas{
+	public abstract void mostrarTablaDeVuelosDisponiblesPorAerolineas(ArrayList<Aerolinea> aerolineas);
+	public abstract void mostrarTablaDeVuelosPorAerolineas(ArrayList<Aerolinea> aerolineas);
+	public abstract void mostrarTablaDeVuelos(Aerolinea aerolineas, ArrayList<Vuelo> vuelos);
+	public abstract void mostrarTablaDePasajeros(ArrayList<Tiquete> tiquetes);
+	public abstract void mostrarTablaDeAerolineas(ArrayList<Aerolinea> aerolineas);
+	public abstract void mostrarTablaDeAlojamientos(ArrayList<Alojamiento> alojamientos);
+}
